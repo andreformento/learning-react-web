8885683a58517794b950a5d8119245ca33dd557c
André Formento <andreformento.sc@gmail.com>
2023-11-12 19:19:57 -0300

    financial-hub

---
+++ next-auth-example/app/financial-hub/page.tsx
@@ -0,0 +1,12 @@
+import { auth } from "auth"
+
+export default async function Page() {
+  return (
+    <div className="space-y-2">
+      <h1 className="text-3xl font-bold">Financial Hub</h1>
+      <p>
+        Only logged
+      </p>
+    </div>
+  )
+}
